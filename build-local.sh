296cbd582fbc4b6333c8068525ca973357e10c38
perlinpinpin <bruno.merlin@gmail.com>
2024-10-29 11:42:13 -0300

    Novos  ajustes na instalação

--- build-local.sh
+++ build-local.sh
@@ -2,8 +2,11 @@
 
 # install && build do frontend
 cd frontend
+mv .env .tmp
+echo "VITE_URL=http://localhost:3008/api/v1" > .env
 npm install
 npm run build
+mv .tmp .env
 cd ..
 
 # Criação da distribuição
@@ -15,11 +18,9 @@ mkdir dist/public/ui
 cp -r frontend/dist/* dist/public/ui
 cp -r frontend/public/* dist/public
 
-sed -r "s/port=(.*)/port=3008/" backend/.env > dist/.env
+sed -r "s/port=(.*)/port=3008/" backend/.env | sed -r "s/HOST_ROOT=(.*)/HOST_ROOT=http:\\/\\/localhost:3008/" > dist/.env
 
 # Inicialização do servidor
 cd dist
 pm2 delete all
-pm2 start servidor.js
-cd ..
-pm2 monit
+nodemon servidor.js
